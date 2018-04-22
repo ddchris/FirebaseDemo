2e8e8211ca44c7157674f1ec23888bb49c957ead
chris2012 <evilkillerice@gmail.com>
2018-04-22 23:13:12 +0800

    e

--- chatroom.js
+++ chatroom.js
@@ -44,14 +44,14 @@ let app = new Vue({
                     this.dialogue += `<li class="${this.myStyle}">
                                         <span>
                                             ${nickname}| ${content}   
-                                            <img src="https://ddchris.github.io/FirebaseDemo/rabbit.jpg" alt=""/>  
+                                            <img src="./rabbit.jpg" alt=""/>  
                                         </span>
                                       </li> `;
                 }
                 else {
                     this.dialogue += `<li class="${this.otherStyle}"> 
                                         <span>
-                                            <img src="https://ddchris.github.io/FirebaseDemo/bear.jpg" alt=""/>    
+                                            <img src="./bear.jpg" alt=""/>    
                                             ${content}| ${nickname}                                    
                                         </span> 
                                       </li> `;
